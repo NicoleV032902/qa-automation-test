15d42072d1fbedff06dcc3407a734c39feb8a909
Nicole Villamor <nicolev@n-compass.online>
2024-05-17 16:05:07 +0800

    added util

--- Dashboard/utilities/baseClasses.py
+++ Dashboard/utilities/baseClasses.py
@@ -1,7 +1,8 @@
 import pytest
 
 # putting all the fixtures in 1 file. this is where the parent class will be stored. All the child class that will be
-# executed in the next file will inherit this parents once it is called and don't need to define @pytest-- redundantly
+# executed in the next file will inherit this parents once it is called and don't need to define
+# @pytest fixture-- redundantly
 
 
 @pytest.mark.usefixtures("setting_browser")
@@ -9,16 +10,16 @@ class SetupBrowser:
     pass
 
 
-# @pytest.mark.userfixtures("Admin_Login")
-# class AdminLogin:
-#     pass
-#
-#
-# @pytest.mark.userfixtures("DealerAdmin_Login")
-# class DealerAdminLogin:
-#     pass
-#
-#
-# @pytest.mark.userfixtures("Dealer_Login")
-# class DealerLogin:
-#     pass
+@pytest.mark.usefixtures("Admin_Login")
+class AdminLogin:
+    pass
+
+
+@pytest.mark.usefixtures("DealerAdmin_Login")
+class DealerAdminLogin:
+    pass
+
+
+@pytest.mark.usefixtures("Dealer_Login")
+class DealerLogin:
+    pass
